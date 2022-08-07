1340e8670fd71bd1fcf015ba71479376d561fc9c
Suvigya <suvigyajaiswal712@gmail.com>
2022-08-07 19:46:54 +0530

    Added Constants for Error Message and Domain URL

---
+++ src/main/java/com/lowe/urlshortener/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.lowe.urlshortener.constants;
+
+public class Constants {
+
+    public static final String DOMAIN_NAME="http://localhost:8080/";
+
+    public static final String BAD_REQUEST_ERROR_MESSAGE = "There was an error processing your request. please try "
+            + "again.";
+    public static final String INTERNAL_SERVER_ERROR = "Internal server error";
+
+    public static final String HTTPS = "https://";
+
+    public static final String HTTP = "http";
+}
